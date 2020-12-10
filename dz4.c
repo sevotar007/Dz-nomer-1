650e8760e4bbac0d40d8d2ea4a6289497fbd6ef4
sevotar007 <56293528+sevotar007@users.noreply.github.com>
2020-12-10 04:01:46 +0300

    Update dz4.c

--- dz4.c
+++ dz4.c
@@ -5,37 +5,37 @@
 #include <inttypes.h>
 
 typedef struct{
-    uint64_t** qraph;
+    uint64_t** graph;
     uint64_t size;
     bool isWeighted;
     bool isDirected;
-} qraph;
+} graph;
 
-qraph* create_qraph(uint64_t size){
-    qraph* new_qraph = (qraph*)malloc(sizeof(qraph));
-    new_qraph->size = size;
-    new_qraph->isWeighted = false;
-    new_qraph->qraph = (uint64_t**)malloc(size*sizeof(uint64_t*));
+graph* create_qraph(uint64_t size){
+    graph* new_graph = (graph*)malloc(sizeof(graph));
+    new_graph->size = size;
+    new_graph->isWeighted = false;
+    new_graph->graph = (uint64_t**)malloc(size*sizeof(uint64_t*));
     for (uint64_t i = 0; i < size; ++i)
-        new_qraph->qraph[i] = (uint64_t*)calloc(size, sizeof(uint64_t));
-    return new_qraph;
+        new_graph->graph[i] = (uint64_t*)calloc(size, sizeof(uint64_t));
+    return new_graph;
 }
 
-void add_edge(qraph* current_qraph, uint64_t from, uint64_t to){
-    current_qraph->qraph[from][to] = 1;
-    if (!current_qraph->isDirected) {
-        current_qraph->qraph[to][from] = 1;
+void add_edge(graph* current_graph, uint64_t from, uint64_t to){
+    current_graph->graph[from][to] = 1;
+    if (!current_graph->isDirected) {
+        current_graph->graph[to][from] = 1;
     }
 }
 
-uint64_t find_minimal_power(qraph* current_qraph){
+uint64_t find_minimal_power(graph* current_graph){
     uint64_t pos = -1 , power = -1;
-    for (uint64_t i = 0; i < current_qraph->size; ++i){
+    for (uint64_t i = 0; i < current_graph->size; ++i){
         uint64_t count_power = 0;
-        for (uint64_t j = 0; j < current_qraph->size; ++j){
-            if (i == j && current_qraph->qraph[i][j]) count_power += 2;
+        for (uint64_t j = 0; j < current_graph->size; ++j){
+            if (i == j && current_graph->graph[i][j]) count_power += 2;
             else {
-                if (current_qraph->qraph[i][j]) {
+                if (current_graph->graph[i][j]) {
                     ++count_power;
                 }
             }
@@ -50,17 +50,17 @@ uint64_t find_minimal_power(qraph* current_qraph){
     return power;
 }
 
-void delete_qraph(qraph* current_qraph){
-    for (uint64_t i = 0; i < current_qraph->size; ++i)
-        free(current_qraph->qraph[i]);
-    free(current_qraph->qraph);
-    free(current_qraph);
+void delete_пraph(graph* current_graph){
+    for (uint64_t i = 0; i < current_graph->size; ++i)
+        free(current_graph->graph[i]);
+    free(current_graph->graph);
+    free(current_graph);
 }
 
-bool check_direction(qraph* current_qraph){
-    for (uint64_t i = 0; i < current_qraph->size; ++i){
-        for (uint64_t j = i; j < current_qraph->size; ++j){
-            if (current_qraph->qraph[i][j] != current_qraph->qraph[j][i]){
+bool check_direction(graph* current_graph){
+    for (uint64_t i = 0; i < current_graph->size; ++i){
+        for (uint64_t j = i; j < current_graph->size; ++j){
+            if (current_graph->graph[i][j] != current_graph->graph[j][i]){
                 return true;
             }
         }
@@ -68,33 +68,33 @@ bool check_direction(qraph* current_qraph){
     return false;
 }
 
-void input_qraph(qraph* current_qraph){
+void input_graph(graph* current_graph){
     printf("Input matrix:");
-    for (uint64_t i = 0; i < current_qraph->size; ++i)
-        for (uint64_t j = 0; j < current_qraph->size; ++j){
-            scanf("%" SCNu64 "", &current_qraph->qraph[i][j]);
-            if (current_qraph->qraph[i][j]>>1)
-                current_qraph->isWeighted = true;
+    for (uint64_t i = 0; i < current_graph->size; ++i)
+        for (uint64_t j = 0; j < current_graph->size; ++j){
+            scanf("%" SCNu64 "", &current_graph->graph[i][j]);
+            if (current_graph->graph[i][j]>>1)
+                current_graph->isWeighted = true;
         }
-    current_qraph->isDirected = check_direction(current_qraph);
+    current_graph->isDirected = check_direction(current_graph);
 }
 
-uint64_t print_dot(FILE* fileptr, qraph* current_qraph){
-    if (current_qraph->isDirected) fprintf(fileptr, "di");
+uint64_t print_dot(FILE* fileptr, graph* current_graph){
+    if (current_graph->isDirected) fprintf(fileptr, "di");
 	fprintf(fileptr, "graph graphname {\n");
     uint64_t lines = 0;
-    for (uint64_t i = 0; i < current_qraph->size; ++i){
-        uint64_t j = (current_qraph->isDirected) ? 0 : i;
-        for (; j < current_qraph->size; ++j){
-            if (current_qraph->qraph[i][j]){
+    for (uint64_t i = 0; i < current_graph->size; ++i){
+        uint64_t j = (current_graph->isDirected) ? 0 : i;
+        for (; j < current_graph->size; ++j){
+            if (current_graph->graph[i][j]){
                 fprintf(fileptr, "\t%" PRIu64 " -", i);
-                if (current_qraph->isDirected)
+                if (current_graph->isDirected)
                     fputc('>', fileptr);
                 else
                     fputc('-', fileptr);
                 fprintf(fileptr,  " %" PRIu64 "", j);
-                if(current_qraph->isWeighted)
-                    fprintf(fileptr, " [label=%" PRIu64 "]", current_qraph->qraph[i][j]);
+                if(current_graph->isWeighted)
+                    fprintf(fileptr, " [label=%" PRIu64 "]", current_graph->graph[i][j]);
                 fprintf(fileptr, ";\n");
                 ++lines;
             }
@@ -104,22 +104,22 @@ uint64_t print_dot(FILE* fileptr, qraph* current_qraph){
     return lines;
 }
 
-uint64_t dfs(qraph* current_qraph, bool* color,uint64_t pos){
+uint64_t dfs(graph* current_qraph, bool* color,uint64_t pos){
     uint64_t quantity_of_lines = 1;
     color[pos] = true;
-    for (uint64_t i = 0; i < current_qraph->size; ++i) {
-        if (current_qraph->qraph[pos][i] && !color[i]){
-            quantity_of_lines += dfs(current_qraph, color, i);
+    for (uint64_t i = 0; i < current_graph->size; ++i) {
+        if (current_graph->graph[pos][i] && !color[i]){
+            quantity_of_lines += dfs(current_graph, color, i);
         }
     }
     return quantity_of_lines;
 }
 
-bool check_connecions(qraph* current_qraph) {
-    bool* color = (bool*)calloc(current_qraph->size, sizeof(bool));
-    uint64_t quantity_of_lines = dfs(current_qraph,color,0);
+bool check_connecions(graph* current_graph) {
+    bool* color = (bool*)calloc(current_graph->size, sizeof(bool));
+    uint64_t quantity_of_lines = dfs(current_graph,color,0);
     free(color);
-    if (quantity_of_lines < current_qraph->size)
+    if (quantity_of_lines < current_graph->size)
         return false;
     else 
         return true;
@@ -129,9 +129,9 @@ int main(int argc, char *argv[]){
     printf("Quantity of centers:\n");
     uint64_t quantity;
     scanf("%" SCNu64 "", &quantity);
-    qraph* qraph = create_qraph(quantity);
-    input_qraph(qraph);
-    if (check_connecions(qraph)) {
+    graph* graph = create_graph(quantity);
+    input_graph(qraph);
+    if (check_connecions(graph)) {
         printf("Qraph is connecting\n");//
     } else {
         printf("Qraph is not connecting\n");
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]){
     scanf("%" SCNu64 "", &add_edges);
     for (uint64_t i = 0; i < add_edges; ++i){
         scanf("%" SCNu64 " %"SCNu64 "", &from, &to);
-        add_edge(qraph, from, to);
+        add_edge(graph, from, to);
         puts("Edge is added in graph");
     }
     printf("Minimal power of vertex is %" PRIu64 "\n", find_minimal_power(qraph));
@@ -166,6 +166,6 @@ int main(int argc, char *argv[]){
     printf("%s\n", term);
     //system(term);
     free(term);
-    delete_qraph(qraph);
+    delete_graph(graph);
     return 0;
 }
